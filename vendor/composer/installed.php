6d5cf3aea22a1b1564412e5577e4b6d57f82d336
yanntyb <yanntyb.lbc@gmail.com>
2022-01-10 09:15:29 +0100

    final

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a78bef0621e548d714ccf877119b8f012b89bcc7',
+        'name' => 'yanntyb/router',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yanntyb/router' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a78bef0621e548d714ccf877119b8f012b89bcc7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
